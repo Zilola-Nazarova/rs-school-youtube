985e3fccaa0e98bb37fd8dd0b0d80d940e88648c
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-07-07 23:05:25 +0500

    feat: create a service to provide json data (instead of using hardcoded json in component)

---
+++ src/app/youtube.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { SearchResults } from './search-results.model';
+import { SearchItem } from './search-item.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YouTubeService {
+  url = 'http://localhost:3000/response';
+  async getVideos() : Promise<SearchResults> {
+    const data = await fetch(`${this.url}`);
+    return await data.json() ?? {};
+  }
+  constructor() { }
+}
