e1714d7c8a36659425afe8cd17aed746c2838332
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-07-07 21:41:29 +0500

    feat: add content to HeaderComponent

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { SortingBarComponent } from '../sorting-bar/sorting-bar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    UserProfileComponent,
+    SearchBarComponent,
+    SortingBarComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
