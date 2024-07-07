b5ed26b6481bbdd179867890088706ca6d40de3e
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-07-07 21:37:12 +0500

    feat: generate necessary components using ng cli

---
+++ src/app/sorting-bar/sorting-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sorting-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sorting-bar.component.html',
+  styleUrl: './sorting-bar.component.scss'
+})
+export class SortingBarComponent {
+
+}
