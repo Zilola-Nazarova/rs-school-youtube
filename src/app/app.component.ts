0ae797e289561080b4c1e2f1d61389043e55557c
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-07-07 21:40:57 +0500

    feat: add content to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SearchResultsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
